368a5d44201da10ff817f15265ad1d7fc4031355
unknown <phamgerard69@gmail.com>
2026-01-27 07:29:46 +0100

    remove some python modules for solvers

--- src/benchmark_solvers/tsp/benchmark/tsp_dashboard.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-runner = TSPBenchmarkRunner(distance_matrices, start=0, runs=5)
-results = runner.run()
-
-df = pd.DataFrame(results)
-df = df.sort_values("rating", ascending=False)
-print(df)
